aa64fcb225b5a4fb84f80f1549db6f1d00d7a513
gcht <gab.github44@gmail.com>
2025-09-16 02:05:58 +0200

    Add opcode 0x40 - 0x4f

--- cpu8080.c
+++ cpu8080.c
@@ -386,197 +386,197 @@ int execute(CPU *cpu)
             cpu->cy ^= 1;
             return 4;
         case 0x40: // MOV B, B
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->b;
+            return 5;
         case 0x41: // MOV B, C
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->c;
+            return 5;
         case 0x42: // MOV B, D
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->d;
+            return 5;
         case 0x43: // MOV B, E
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->e;
+            return 5;
         case 0x44: // MOV B, H
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->h;
+            return 5;
         case 0x45: // MOV B, L
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->l;
+            return 5;
         case 0x46: // MOV B, M
-            // TODO: implémentation
-            break;
+            cpu->b = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x47: // MOV B, A
-            // TODO: implémentation
-            break;
+            cpu->b = cpu->a;
+            return 5;
         case 0x48: // MOV C, B
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->b;
+            return 5;
         case 0x49: // MOV C, C
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->c;
+            return 5;
         case 0x4A: // MOV C, D
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->d;
+            return 5;
         case 0x4B: // MOV C, E
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->e;
+            return 5;
         case 0x4C: // MOV C, H
-            // TODO: implémentation
-            break;
+             cpu->c = cpu->h;
+            return 5;
         case 0x4D: // MOV C, L
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->l;
+            return 5;
         case 0x4E: // MOV C, M
-            // TODO: implémentation
-            break;
+            cpu->c = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x4F: // MOV C, A
-            // TODO: implémentation
-            break;
+            cpu->c = cpu->a;
+            return 5;
         case 0x50: // MOV D, B
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->b;
+            return 5;
         case 0x51: // MOV D, C
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->c;
+            return 5;
         case 0x52: // MOV D, D
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->d;
+            return 5;
         case 0x53: // MOV D, E
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->e;
+            return 5;
         case 0x54: // MOV D, H
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->h;
+            return 5;
         case 0x55: // MOV D, L
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->l;
+            return 5;
         case 0x56: // MOV D, M
-            // TODO: implémentation
-            break;
+            cpu->d = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x57: // MOV D, A
-            // TODO: implémentation
-            break;
+            cpu->d = cpu->a;
+            return 5;
         case 0x58: // MOV E, B
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->b;
+            return 5;
         case 0x59: // MOV E, C
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->c;
+            return 5;
         case 0x5A: // MOV E, D
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->d;
+            return 5;
         case 0x5B: // MOV E, E
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->e;
+            return 5;
         case 0x5C: // MOV E, H
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->h;
+            return 5;
         case 0x5D: // MOV E, L
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->l;
+            return 5;
         case 0x5E: // MOV E, M
-            // TODO: implémentation
-            break;
+            cpu->e = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x5F: // MOV E, A
-            // TODO: implémentation
-            break;
+            cpu->e = cpu->a;
+            return 5;
         case 0x60: // MOV H, B
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->b;
+            return 5;
         case 0x61: // MOV H, C
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->c;
+            return 5;
         case 0x62: // MOV H, D
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->d;
+            return 5;
         case 0x63: // MOV H, E
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->e;
+            return 5;
         case 0x64: // MOV H, H
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->h;
+            return 5;
         case 0x65: // MOV H, L
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->l;
+            return 5;
         case 0x66: // MOV H, M
-            // TODO: implémentation
-            break;
+             cpu->h = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x67: // MOV H, A
-            // TODO: implémentation
-            break;
+            cpu->h = cpu->a;
+            return 5;
         case 0x68: // MOV L, B
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->b;
+            return 5;
         case 0x69: // MOV L, C
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->c;
+            return 5;
         case 0x6A: // MOV L, D
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->d;
+            return 5;
         case 0x6B: // MOV L, E
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->e;
+            return 5;
         case 0x6C: // MOV L, H
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->h;
+            return 5;
         case 0x6D: // MOV L, L
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->l;
+            return 5;
         case 0x6E: // MOV L, M
-            // TODO: implémentation
-            break;
+            cpu->l = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x6F: // MOV L, A
-            // TODO: implémentation
-            break;
+            cpu->l = cpu->a;
+            return 5;
         case 0x70: // MOV M, B
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->b);
+            return 7;
         case 0x71: // MOV M, C
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->c);
+            return 7;
         case 0x72: // MOV M, D
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->d);
+            return 7;
         case 0x73: // MOV M, E
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->e);
+            return 7;
         case 0x74: // MOV M, H
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->h);
+            return 7;
         case 0x75: // MOV M, L
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->l);
+            return 7;
         case 0x76: // HLT
             // TODO: implémentation
             break;
         case 0x77: // MOV M, A
-            // TODO: implémentation
-            break;
+            write_memory(cpu, ((cpu->h << 8) | cpu->l), cpu->a);
+            return 7;
         case 0x78: // MOV A, B
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->b;
+            return 5;
         case 0x79: // MOV A, C
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->c;
+            return 5;
         case 0x7A: // MOV A, D
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->d;
+            return 5;
         case 0x7B: // MOV A, E
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->e;
+            return 5;
         case 0x7C: // MOV A, H
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->h;
+            return 5;
         case 0x7D: // MOV A, L
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->l;
+            return 5;
         case 0x7E: // MOV A, M
-            // TODO: implémentation
-            break;
+            cpu->a = read_memory(cpu, ((cpu->h << 8) | cpu->l));
+            return 7;
         case 0x7F: // MOV A, A
-            // TODO: implémentation
-            break;
+            cpu->a = cpu->a;
+            return 5;
         case 0x80: // ADD B
             // TODO: implémentation
             break;
