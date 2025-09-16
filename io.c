96935ec1a6da8d3b2d846dea148b3a5185e56dd3
gcht <gab.github44@gmail.com>
2025-09-16 22:44:40 +0200

    Add printf OUT

--- io.c
+++ io.c
@@ -1,11 +1,31 @@
 #include "includes/io.h"
 
+#include <stdio.h>
+
 void write_io(uint8_t port, uint8_t value)
 {
+    switch (port)
+    {
+    case 0x00:
+        printf("%c", value);
+        return;
+    case 0x01:
+        printf("%c", value);
+        return;
+    default:
+        break;
+    }
     return;
 }
 
 uint8_t read_io(uint8_t port)
 {
+    switch (port)
+    {
+    case 0x00:
+        break;
+    default:
+        break;
+    }
     return 0;
 }
