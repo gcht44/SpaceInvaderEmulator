ee90c70a3160276a113087256d519feb533d0eee
gcht <gab.github44@gmail.com>
2025-09-16 20:19:48 +0200

    Add small io implementation

---
+++ includes/io.h
@@ -0,0 +1,10 @@
+#ifndef IO__H
+#define IO__H
+
+#include <stdint.h>
+
+// typedef struct CPU CPU;
+
+void write_io(uint8_t port, uint8_t value);
+
+#endif
---
+++ io.c
@@ -0,0 +1,6 @@
+#include "includes/io.h"
+
+void write_io(uint8_t port, uint8_t value)
+{
+    return;
+}
